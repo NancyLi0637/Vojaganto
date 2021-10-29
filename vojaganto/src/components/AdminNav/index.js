b75c2e5ea06b156732a51dd190577dce7dae610e
Bobby <leavewsoul@gmail.com>
2021-10-29 00:17:49 -0400

    change some possible style conflict in admin home, finish sketch version of admin login

--- vojaganto/src/components/AdminNav/index.js
+++ vojaganto/src/components/AdminNav/index.js
@@ -7,21 +7,21 @@ import "./style.scss";
 class AdminNav extends React.Component {
 
     render() {
-        const { current_userid } = this.props
+        const { current_adminid } = this.props
         return (
             <div>
                 <div className="admin-nav">
-                    <Link className="admin-nav-item" to={`/admin-home/${current_userid}`}>
+                    <Link className="admin-nav-item" to={`/admin-home/${current_adminid}`}>
                         <img src="https://img.icons8.com/material-outlined/24/ffffff/home--v2.png"/>
                         <span>Home</span>
                     </Link>
                     <br />
-                    <Link className="admin-nav-item" to={`/admin-user-dash/${current_userid}`}>
+                    <Link className="admin-nav-item" to={`/admin-user-dash/${current_adminid}`}>
                         <img src="https://img.icons8.com/material-outlined/24/ffffff/user--v1.png"/>
                         <span>Manage User</span>
                     </Link>
                     <br />
-                    <Link className="admin-nav-item" to={`/admin-post-dash/${current_userid}`}>
+                    <Link className="admin-nav-item" to={`/admin-post-dash/${current_adminid}`}>
                         <img src="https://img.icons8.com/ios/50/ffffff/journal.png"/>
                         Manage Post
                     </Link>
--- vojaganto/src/pages/AdminHome/index.js
+++ vojaganto/src/pages/AdminHome/index.js
@@ -6,13 +6,13 @@ import './style.scss'
 
 class AdminHome extends React.Component{
     render(){
-        const { current_userid } = this.props
+        const { current_adminid } = this.props
 
         return (
             <div class="page admin-home-page">
                 <div className="admin-home-main">
-                    <AdminNav/>
-                    <h1>Welcome Administor {`${current_userid}`}</h1>
+                    <AdminNav current_userid={current_adminid}/>
+                    <h1>Welcome Administor {`${current_adminid}`}</h1>
                 </div>
             </div>
         )
