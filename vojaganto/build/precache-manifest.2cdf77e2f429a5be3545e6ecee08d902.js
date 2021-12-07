061b570b631345f3d49dd3438c284fbcfb9295a1
rainyuxuan <yu2001xuan@outlook.com>
2021-12-07 00:01:47 -0500

    Heroku builds

---
+++ vojaganto/build/precache-manifest.2cdf77e2f429a5be3545e6ecee08d902.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f09ab55ec31f76107c75987133643661",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04c532b73c3bf9d2eb58",
+    "url": "/static/css/main.d0e1335f.chunk.css"
+  },
+  {
+    "revision": "4a6ef4deab17381d8f81",
+    "url": "/static/js/2.704d8bae.chunk.js"
+  },
+  {
+    "revision": "6f989b3e63e771193900b877cc20862b",
+    "url": "/static/js/2.704d8bae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48fb3ef2fc062166932e",
+    "url": "/static/js/3.976a366c.chunk.js"
+  },
+  {
+    "revision": "04c532b73c3bf9d2eb58",
+    "url": "/static/js/main.594f8e25.chunk.js"
+  },
+  {
+    "revision": "9095b257b23ce1087718",
+    "url": "/static/js/runtime-main.3d65df96.js"
+  },
+  {
+    "revision": "afe789093ee5e4e7a6fc6eb36778955a",
+    "url": "/static/media/background.afe78909.jpg"
+  }
+]);
