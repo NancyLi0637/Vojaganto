25cef3ceae3eaef9183ad4d67399968f20651f7c
Bobby <leavewsoul@gmail.com>
2021-12-07 17:57:11 -0500

    fix update user profile

--- services/user/index.js
+++ services/user/index.js
@@ -12,10 +12,10 @@ class UserService {
      * @param {User} user 
      * @returns same user object as input but remove password
      */
-    _returnStyle(user){
+    _returnStyle(user) {
         let updatedUser = {}
-        for(let key of Object.keys(user)){
-            if(key !== "password"){
+        for (let key of Object.keys(user)) {
+            if (key !== "password") {
                 updatedUser[key] = user[key]
             }
         }
@@ -46,6 +46,9 @@ class UserService {
      */
     async getUser(uid) {
         let user = await User.findById(uid).exec()
+        if (!user) {
+            throw { msg: "Not Found: User not found!", status: 404 }
+        }
         let result = this._returnStyle(user._doc)
         logger.log(`Get User [${user.username}]`)
         return result
@@ -74,10 +77,13 @@ class UserService {
         }
         // Validate if the new journey is valid
         // NOTE: the input of a defaultJourney should be a journey id that is already created
-        let newDefaultJourney = await Journey.findById(data.defaultJourney).exec()
-        if (!newDefaultJourney){
-            return "journey not found"
+        if (data.defaultJourney) {
+            let newDefaultJourney = await Journey.findById(data.defaultJourney).exec()
+            if (!newDefaultJourney) {
+                return "journey not found"
+            }
         }
+
         let user = await User.findByIdAndUpdate(uid, data, { new: true }).exec()
         let result = this._returnStyle(user._doc)
         logger.log(`Modify User [${user.username}]`)
@@ -114,13 +120,13 @@ class UserService {
     async login(username, password) {
         let user = await User.findOne({ username: username }).exec()
         if (!user) {
-            throw { msg: "Login Failed! Username not found!", status: 400}
+            throw { msg: "Login Failed! Username not found!", status: 400 }
         }
         if (!bcrypt.compareSync(password, user.password)) {
-            throw { msg: "Login Failed! Please check your username and password!", status: 400}
+            throw { msg: "Login Failed! Please check your username and password!", status: 400 }
         }
         if (!user.active) {
-            throw { msg: "Login Failed! This account is currently banned", status: 403}
+            throw { msg: "Login Failed! This account is currently banned", status: 403 }
         }
         user = await User.findByIdAndUpdate(user._id, { lastLogin: Date.now() }, { new: true }).exec()
         logger.log(`Login User [${user.username}]`)
@@ -133,13 +139,13 @@ class UserService {
 
 
 
-    async getUserJourney(userId, uid){
+    async getUserJourney(userId, uid) {
         // Find all the journeys of the user
-        let allJourney = await Journey.find({"author": uid}).exec()
+        let allJourney = await Journey.find({ "author": uid }).exec()
 
         // Return the result journey in the appropriate data structure
         let userJourney = []
-        for (let eachJourney of allJourney){
+        for (let eachJourney of allJourney) {
             userJourney.push(await journeyService.getReturnedJourneyField(userId, eachJourney))
         }
         logger.log(`Get user journey`)
@@ -147,10 +153,10 @@ class UserService {
 
     }
 
-    async createUserJourney(userId, data){
+    async createUserJourney(userId, data) {
         // Check if the journey name already exist
-        let currJourney = await Journey.find({"title": data["title"], "author": data["author"]}).exec()
-        if (currJourney.length > 0){
+        let currJourney = await Journey.find({ "title": data["title"], "author": data["author"] }).exec()
+        if (currJourney.length > 0) {
             return "repeat"
         }
         // Create the journey
@@ -160,15 +166,15 @@ class UserService {
         let res = await journeyService.getReturnedJourneyField(userId, createdJourney)
         logger.log(`Create Journey [${createdJourney.title}]`)
         return res
- 
+
     }
 
-    async getUserPosting(userId, uid){
+    async getUserPosting(userId, uid) {
         // Get all the journey of the user
-        let allJourney = await Journey.find({"author": uid}).exec()
+        let allJourney = await Journey.find({ "author": uid }).exec()
         // Return the appropriate data strcture (contianing all the postings for the journey)
         let res = []
-        for (let eachJourney of allJourney){
+        for (let eachJourney of allJourney) {
             let currJourney = await journeyService.getReturnedJourneyField(userId, eachJourney)
             res.push(currJourney)
         }
